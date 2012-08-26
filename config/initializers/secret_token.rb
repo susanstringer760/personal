1d152810ac3e596367729254a48686f593a33d9b
Susan Stringer <snorman@ucar.edu>
2012-08-26 13:42:07 -0600

    Initial commit of personal rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Personal::Application.config.secret_token = 'a454d929572ff6c31298d42c2a24c5e1bffba31ab96c8f26c686fae1fc93ad2f042c977f506d8a5a071d2d6c9992937f791f6c3f5c9290d1a41d1da3d4d22655'
